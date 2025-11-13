64a51abe55976fa096b0922b98387fbc527d0d8c
Susana016 <shernandez226@my.gcu.edu>
2025-11-12 19:50:16 -0700

    Added it into a int main(){}

--- CST-405-minimal/test_features.c
+++ CST-405-minimal/test_features.c
@@ -1,36 +1,39 @@
-int x = 10;
-int y = 3;
+int main() {
 
-int sum = x + y;
-int diff = x - y;
-int prod = x * y;
-int quot = x / y;
-int mod = x % y;
+    int x = 10;
+    int y = 3;
 
-print(sum);
-print(diff);
-print(prod);
-print(quot);
-print(mod);
+    int sum = x + y;
+    int diff = x - y;
+    int prod = x * y;
+    int quot = x / y;
+    int mod = x % y;
 
-int choice = 2;
-int result;
+    print(sum);
+    print(diff);
+    print(prod);
+    print(quot);
+    print(mod);
 
-switch (choice) {
-    case 1:
-        result = 100;
-        print(result);
-        break;
-    case 2:
-        result = 200;
-        print(result);
-        break;
-    case 3:
-        result = 300;
-        print(result);
-        break;
-    default:
-        result = 999;
-        print(result);
-        break;
+    int choice = 2;
+    int result;
+
+    switch (choice) {
+        case 1:
+            result = 100;
+            print(result);
+            break;
+        case 2:
+            result = 200;
+            print(result);
+            break;
+        case 3:
+            result = 300;
+            print(result);
+            break;
+        default:
+            result = 999;
+            print(result);
+            break;
+    }
 }
