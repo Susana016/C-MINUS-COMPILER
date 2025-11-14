bf6a16122d64f0dbb91761c81cec3089914db75d
Soren Haynes <livery.gambler-7y@icloud.com>
2025-11-14 09:18:23 -0800

    created Bubble Sort program

---
+++ CST-405-minimal/output_swap.s
@@ -0,0 +1,285 @@
+.data
+
+.text
+.globl main
+
+main:
+    # Allocate space for global variables
+    addi $sp, $sp, -400
+    move $s7, $sp    # Save global base pointer in $s7
+
+    # Global array num[5] at offset 0
+    jal _user_main
+
+    # Exit program
+    addi $sp, $sp, 400
+    li $v0, 10
+    syscall
+
+
+# Function: swap returns void
+swap:
+    addi $sp, $sp, -408
+    sw $ra, 404($sp)
+    sw $fp, 400($sp)
+    move $fp, $sp
+    # Declared int temp at offset 0
+    lw $t0, 8($sp)
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    sw $t0, 0($sp)
+    lw $t0, 8($sp)
+    li $t1, 1
+    add $t0, $t0, $t1
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    lw $t1, 8($sp)
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    lw $t0, 0($sp)
+    lw $t1, 8($sp)
+    li $t2, 1
+    add $t1, $t1, $t2
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    move $sp, $fp
+    lw $fp, 400($sp)
+    lw $ra, 404($sp)
+    addi $sp, $sp, 408
+    jr $ra
+
+# Function: main returns int
+_user_main:
+    addi $sp, $sp, -408
+    sw $ra, 404($sp)
+    sw $fp, 400($sp)
+    move $fp, $sp
+    # Declared and initialized int numLength at offset 0
+    li $t0, 5
+    sw $t0, 0($sp)
+    # Declared int x at offset 4
+    # Declared int i at offset 8
+    li $t0, 64
+    li $t1, 0
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    li $t0, 34
+    li $t1, 1
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    li $t0, 25
+    li $t1, 2
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    li $t0, 12
+    li $t1, 3
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    li $t0, 22
+    li $t1, 4
+    # Array assignment: num[index] = value
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    sw $t0, 0($t1)
+    li $t0, 0
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 1
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 2
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 3
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 4
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 0
+    sw $t0, 8($sp)
+Lwhile_0:
+    lw $t0, 8($sp)
+    lw $t1, 0($sp)
+    slt $t0, $t0, $t1
+    # while condition result in $t0
+    beq $t0, $zero, Lend_while_0
+    li $t0, 0
+    sw $t0, 4($sp)
+Lwhile_1:
+    lw $t0, 4($sp)
+    lw $t1, 0($sp)
+    li $t2, 1
+    sub $t1, $t1, $t2
+    slt $t0, $t0, $t1
+    # while condition result in $t0
+    beq $t0, $zero, Lend_while_1
+    lw $t0, 4($sp)
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    lw $t1, 4($sp)
+    li $t2, 1
+    add $t1, $t1, $t2
+    # Array access: num[index]
+    sll $t1, $t1, 2
+    add $t1, $t1, $sp
+    lw $t1, 0($t1)
+    slt $t0, $t1, $t0
+    # if condition result in $t0
+    beq $t0, $zero, Lend_if_0
+    lw $t0, 4($sp)
+    move $a0, $t0
+    jal swap
+Lend_if_0:
+    lw $t1, 4($sp)
+    li $t2, 1
+    add $t1, $t1, $t2
+    sw $t1, 4($sp)
+    j Lwhile_1
+Lend_while_1:
+    lw $t0, 8($sp)
+    li $t1, 1
+    add $t0, $t0, $t1
+    sw $t0, 8($sp)
+    j Lwhile_0
+Lend_while_0:
+    li $t0, 0
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 1
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 2
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 3
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 4
+    # Array access: num[index]
+    sll $t0, $t0, 2
+    add $t0, $t0, $sp
+    lw $t0, 0($t0)
+    # Print integer
+    move $a0, $t0
+    li $v0, 1
+    syscall
+    # Print newline
+    li $v0, 11
+    li $a0, 10
+    syscall
+    li $t0, 0
+    move $v0, $t0
+    move $sp, $fp
+    lw $fp, 400($sp)
+    lw $ra, 404($sp)
+    addi $sp, $sp, 408
+    jr $ra
---
+++ CST-405-minimal/test_while_array_swap.c
@@ -0,0 +1,56 @@
+/* Test while loops with arrays and swap function */
+/* This program demonstrates bubble sort using while and if statements */
+
+int num[5];
+
+/* Swap function that swaps two adjacent elements in the global array */
+void swap(int idx) {
+    int temp;
+    temp = num[idx];
+    num[idx] = num[idx + 1];
+    num[idx + 1] = temp;
+    return;
+}
+
+int main() {
+    int numLength = 5;
+    int x;
+    int i;
+
+    /* Initialize array with unsorted values */
+    num[0] = 64;
+    num[1] = 34;
+    num[2] = 25;
+    num[3] = 12;
+    num[4] = 22;
+
+    /* Print original array */
+    print(num[0]);
+    print(num[1]);
+    print(num[2]);
+    print(num[3]);
+    print(num[4]);
+
+    /* Bubble sort using while loop and if statements */
+    i = 0;
+    while (i < numLength) {
+        x = 0;
+        while (x < numLength - 1) {
+            /* If current element is greater than next, swap them */
+            if (num[x] > num[x + 1]) {
+                swap(x);
+            }
+            x = x + 1;
+        }
+        i = i + 1;
+    }
+
+    /* Print sorted array */
+    print(num[0]);
+    print(num[1]);
+    print(num[2]);
+    print(num[3]);
+    print(num[4]);
+
+    return 0;
+}
